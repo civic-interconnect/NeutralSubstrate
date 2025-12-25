4004344f9ffcb9ede1635c5d6f500a58bdd1bec1
Lean 4 VS Code Extension <>
2025-12-25 06:12:57 -0600

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import NeutralSubstrate
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ NeutralSubstrate.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `NeutralSubstrate` library.
+-- Import modules here that should be built as part of the library.
+import NeutralSubstrate.Basic
